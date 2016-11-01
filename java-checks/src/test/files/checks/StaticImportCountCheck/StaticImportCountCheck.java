016b77c4f3e749f6f27897cdf556c1eb591e1d20
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2016-11-01 17:17:11 +0100

    SONARJAVA-1875 Rule S3030: Classes should not have too many "static" imports

---
+++ java-checks/src/test/files/checks/StaticImportCountCheck/StaticImportCountCheck.java
@@ -0,0 +1,5 @@
+import static java.lang.Math.*;
+import static java.util.Collections.*;
+import static com.myco.corporate.Constants.*;
+import static com.myco.division.Constants.*;
+import static com.myco.department.Constants.*;  // Noncompliant
